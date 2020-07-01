2f12ebcad61edae5afd1d7ec1ee184906e851186
titouane74 <57191853+titouane74@users.noreply.github.com>
2020-07-01 17:54:25 +0200

    Add Go4Lunch logo application Start authentification development - with email - with Google Add Authentification dependency

--- app/src/main/java/com/fleb/go4lunch/MainActivity.java
+++ app/src/main/java/com/fleb/go4lunch/MainActivity.java
@@ -3,12 +3,43 @@ package com.fleb.go4lunch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 123;
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonLogin = findViewById(R.id.main_activity_button_login);
+        buttonLogin.setOnClickListener(view -> this.startSignInActivity());
+    }
+
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),
+                                        new AuthUI.IdpConfig.GoogleBuilder().build()
+//                                        ,
+//                                        new AuthUI.IdpConfig.FacebookBuilder().build()
+                                        //,
+//                                        new AuthUI.IdpConfig.TwitterBuilder().build()
+                                ))
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_go4lunch_logo)
+                        .build(),
+                RC_SIGN_IN);
     }
 }
