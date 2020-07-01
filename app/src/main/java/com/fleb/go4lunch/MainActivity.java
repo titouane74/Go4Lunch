ec78c4dec6182bc5929313c856b4aab0aa36e6a6
titouane74 <57191853+titouane74@users.noreply.github.com>
2020-07-02 01:46:06 +0200

    Authentification with email, Google and Facebook implemented

--- app/src/main/java/com/fleb/go4lunch/MainActivity.java
+++ app/src/main/java/com/fleb/go4lunch/MainActivity.java
@@ -1,17 +1,27 @@
 package com.fleb.go4lunch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 
 import java.util.Arrays;
+import java.util.Objects;
+
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
+    private Toast mToast;
+
     Button buttonLogin;
 
     @Override
@@ -24,6 +34,40 @@ public class MainActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(view -> this.startSignInActivity());
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                mToast = Toast.makeText(this,getString(R.string.connection_succeed),Toast.LENGTH_SHORT);
+                mToast.show();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+
+                if (response == null) {
+                    mToast = Toast.makeText(this,getString(R.string.error_authentication_canceled),Toast.LENGTH_SHORT);
+                    mToast.show();
+                } else if (Objects.requireNonNull(response.getError()).getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    mToast = Toast.makeText(this,getString(R.string.error_no_internet),Toast.LENGTH_SHORT);
+                    mToast.show();
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    mToast = Toast.makeText(this,getString(R.string.error_unknown_error),Toast.LENGTH_SHORT);
+                    mToast.show();
+                }
+            }
+        }
+    }
+
     private void startSignInActivity(){
         startActivityForResult(
                 AuthUI.getInstance()
@@ -32,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAvailableProviders(
                                 Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),
                                         new AuthUI.IdpConfig.GoogleBuilder().build()
-//                                        ,
-//                                        new AuthUI.IdpConfig.FacebookBuilder().build()
+                                        ,
+                                        new AuthUI.IdpConfig.FacebookBuilder().build()
                                         //,
 //                                        new AuthUI.IdpConfig.TwitterBuilder().build()
                                 ))
