d1d2e66e87a7bd2d9ad7fa87caba7836ac8a95eb
titouane74 <57191853+titouane74@users.noreply.github.com>
2020-09-21 20:08:01 +0200

    MainActivity : Implements data in the drawer layout

--- app/src/main/java/com/fleb/go4lunch/view/activities/AuthenticationActivity.java
+++ app/src/main/java/com/fleb/go4lunch/view/activities/AuthenticationActivity.java
@@ -97,7 +97,6 @@ public class AuthenticationActivity extends AppCompatActivity implements View.On
         if (mCurrentUser != null) {
             updateUI(mCurrentUser);
         }
-
     }
 
     @Override
--- app/src/main/java/com/fleb/go4lunch/view/activities/MainActivity.java
+++ app/src/main/java/com/fleb/go4lunch/view/activities/MainActivity.java
@@ -1,8 +1,13 @@
 package com.fleb.go4lunch.view.activities;
 //TODO implement the javadoc
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -12,11 +17,16 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.auth.AuthUI;
 
 import com.fleb.go4lunch.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class MainActivity extends BaseActivity {
 
@@ -26,13 +36,22 @@ public class MainActivity extends BaseActivity {
     private static final String TAG_MAIN = "TAG_MAIN";
     private NavController mNavController;
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mCurrentUser;
+
     @Override
-    public int getActivityLayout() { return R.layout.activity_main; }
+    public int getActivityLayout() {
+        return R.layout.activity_main;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getActivityLayout());
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mFirebaseAuth.getCurrentUser();
+
         configureToolBar();
         configureDrawerLayoutNavigationView();
 
@@ -85,12 +104,29 @@ public class MainActivity extends BaseActivity {
     private void configureDrawerLayoutNavigationView() {
         mDrawerLayout = findViewById(R.id.main_activity_drawer_layout);
         mNavigationView = findViewById(R.id.nav_view);
+        View lHeaderView = mNavigationView.getHeaderView(0);
         mNavController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        ImageView lImgUser = lHeaderView.findViewById(R.id.img_user);
+        TextView lTxtName = lHeaderView.findViewById(R.id.txt_user_name);
+        TextView lTxtEmail = lHeaderView.findViewById(R.id.txt_user_email);
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_lunch, R.id.nav_logout, R.id.nav_settings, R.id.nav_map, R.id.nav_restaurant_list, R.id.nav_workmate)
                 .setDrawerLayout(mDrawerLayout)
                 .build();
+
+        if (mCurrentUser != null) {
+            String lEmail = mCurrentUser.getEmail();
+            String lName = mCurrentUser.getDisplayName();
+            String lPhotoUrl;
+
+            lPhotoUrl = Objects.requireNonNull(mCurrentUser.getPhotoUrl()).toString();
+            Glide.with(this).load(lPhotoUrl).into(lImgUser);
+            lTxtName.setText(lName);
+            lTxtEmail.setText(lEmail);
+        }
+
     }
 
 }
