4a5d37b762432046d57455d675dbefe7cdca35a8
titouane74 <57191853+titouane74@users.noreply.github.com>
2020-11-15 18:40:59 +0100

    Unit Test : rename RestaurantListViewModelTest in Go4LunchViewModelTest due to the change in the app

--- app/src/test/java/com/fleb/go4lunch/viewmodels/Go4LunchViewModelTest.java
+++ app/src/test/java/com/fleb/go4lunch/viewmodels/Go4LunchViewModelTest.java
@@ -7,7 +7,6 @@ import com.fleb.go4lunch.model.Restaurant;
 import com.fleb.go4lunch.repository.RestaurantRepository;
 import com.fleb.go4lunch.repository.WorkmateRepository;
 import com.fleb.go4lunch.viewmodel.Go4LunchViewModel;
-import com.fleb.go4lunch.viewmodel.RestaurantListViewModel;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -15,8 +14,10 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -24,11 +25,10 @@ import static org.mockito.MockitoAnnotations.initMocks;
 /**
  * Created by Florence LE BOURNOT on 15/11/2020
  */
-public class RestaurantListViewModelTest {
+public class Go4LunchViewModelTest {
 
     @Rule
     public InstantTaskExecutorRule mInstantTaskExecutorRule = new InstantTaskExecutorRule();
-//    private RestaurantListViewModel mRestaurantListViewModel;
     private Go4LunchViewModel mGo4LunchListViewModel;
 
     @Mock
@@ -40,17 +40,29 @@ public class RestaurantListViewModelTest {
     @Mock
     private MutableLiveData<List<Restaurant>> mNewLDRestaurantList = new MutableLiveData<>();
 
+    private List<Restaurant> mRestaurantList = new ArrayList<>();
+
     @Before
     public void setup() {
         initMocks(this);
         mGo4LunchListViewModel = new Go4LunchViewModel(mRestaurantRepository, mWorkmateRepository);
+
+        mRestaurantList .add(new Restaurant("tataid", "tata"));
+        mRestaurantList .add(new Restaurant("julesid", "jules"));
     }
 
     @Test
-    public void getRestauarntListWithSuccess() {
+    public void getRestaurantListWithSuccess() {
         when(mRestaurantRepository.getLDRestaurantList()).thenReturn(mNewLDRestaurantList);
         assertNotNull(mGo4LunchListViewModel.getRestaurantList());
         Mockito.verify(mRestaurantRepository).getLDRestaurantList();
 
+        mRestaurantRepository.getLDRestaurantList().observeForever(pRestaurantList ->
+        {
+            System.out.println(pRestaurantList.size());
+            assertEquals(pRestaurantList,mNewLDRestaurantList);
+            assertEquals(pRestaurantList,mRestaurantList);
+        });
     }
+
 }
