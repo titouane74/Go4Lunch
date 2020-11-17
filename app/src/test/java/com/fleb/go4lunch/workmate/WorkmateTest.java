41bdf78c3b7d2da9ac211b2db5c43479a6379c20
titouane74 <57191853+titouane74@users.noreply.github.com>
2020-11-17 01:51:51 +0100

    Unit Test : Model : Workmate tests

--- app/src/test/java/com/fleb/go4lunch/workmate/WorkmateTest.java
+++ app/src/test/java/com/fleb/go4lunch/workmate/WorkmateTest.java
@@ -1,14 +1,17 @@
 package com.fleb.go4lunch.workmate;
 
-import com.fleb.go4lunch.di.DI;
-import com.fleb.go4lunch.service.Go4LunchApi;
+import com.fleb.go4lunch.model.Workmate;
+import com.google.firebase.Timestamp;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 /**
@@ -16,53 +19,194 @@ import static org.mockito.MockitoAnnotations.initMocks;
  */
 public class WorkmateTest {
 
-    private Go4LunchApi mApi;
+    private Workmate mWorkmate;
+    private String mNoId;
+    private String mNoName;
+    private String mNoEmail;
+    private String mNoPhotoUrl;
+    private Workmate.WorkmateRestoChoice mWorkmateRestoChosen;
+    private List<Workmate.Likes> mWorkmateLikesList = new ArrayList<>();
+    private Workmate.Likes mWorkmateLikes;
 
-    @BeforeEach
+    @Before
     public void setup() {
         initMocks(this);
-        mApi = DI.getGo4LunchApiService();
-        assertThat(mApi, notNullValue());
+        Workmate.Likes lLikes;
+
+        mNoId = "no_id";
+        mNoName = "no_name";
+        mNoEmail = "no_email";
+        mNoPhotoUrl = "no_photo_url";
+    }
+
+    @Test
+    public void setAndGetWorkmateIdWithSuccess() {
+        mWorkmate = new Workmate(mNoId,mNoName);
+        assertEquals(mNoId,mWorkmate.getWorkmateId());
+
+        String lSetData = "albert_id";
+        mWorkmate.setWorkmateId(lSetData);
+
+        String lGetData = mWorkmate.getWorkmateId();
+        assertNotNull(lGetData);
+        assertEquals(lSetData,lGetData);
+    }
+
+    @Test
+    public void setAndGetWorkmateNameWithSuccess() {
+        mWorkmate = new Workmate(mNoId,mNoName);
+        assertEquals(mNoName, mWorkmate.getWorkmateName());
+
+        String lSetData = "albert";
+        mWorkmate.setWorkmateName(lSetData);
+
+        String lGetData = mWorkmate.getWorkmateName();
+        assertNotNull(lGetData);
+        assertEquals(lSetData, lGetData);
 
     }
 
-    @AfterEach
-    public void tearDown() { mApi = DI.getNewInstanceApiService(); }
+    @Test
+    public void setAndGetWorkmateEmailWithSuccess() {
+        mWorkmate = new Workmate(mNoId,mNoName, mNoEmail, mNoPhotoUrl);
+        assertEquals(mNoEmail, mWorkmate.getWorkmateEmail());
+
+        String lSetData = "albert@email.com";
+        mWorkmate.setWorkmateEmail(lSetData);
+
+        String lGetData = mWorkmate.getWorkmateEmail();
+        assertNotNull(lGetData);
+        assertEquals(lSetData, lGetData);
+    }
 
     @Test
-    public void setAndGetWorkmateIdWithSuccess() { }
+    public void setAndGetWorkmatePhotoUrlWithSuccess() {
+        mWorkmate = new Workmate(mNoId,mNoName, mNoEmail, mNoPhotoUrl);
+        assertEquals(mNoPhotoUrl, mWorkmate.getWorkmatePhotoUrl());
+
+        String lSetData = "photo url";
+        mWorkmate.setWorkmatePhotoUrl(lSetData);
+
+        String lGetData = mWorkmate.getWorkmatePhotoUrl();
+        assertNotNull(lGetData);
+        assertEquals(lSetData, lGetData);
+    }
 
     @Test
-    public void setAndGetWorkmateNameWithSuccess() { }
+    public void setAndGetWorkmateLikesWithSuccess() {
+        int lInitialNbLikes = mWorkmateLikesList.size();
+        assertEquals(0, mWorkmateLikesList.size());
+
+        mWorkmate = new Workmate(mNoId, mWorkmateLikesList);
+        assertNotNull(mWorkmate);
+        assertEquals(0,mWorkmate.getWorkmateLikes().size());
+
+        Workmate.Likes lSetLike = new Workmate.Likes("tataid","tataeugenie");
+        mWorkmateLikesList.add(lSetLike);
+        assertEquals(lInitialNbLikes+1, mWorkmateLikesList.size());
+
+        mWorkmate.setWorkmateLikes(mWorkmateLikesList);
+        assertEquals(lInitialNbLikes+1, mWorkmate.getWorkmateLikes().size());
+
+        Workmate.Likes lGetLike = mWorkmate.getWorkmateLikes().get(0);
+        assertEquals(lSetLike,lGetLike);
+
+        mWorkmateLikesList.remove(lSetLike);
+        mWorkmate.setWorkmateLikes(mWorkmateLikesList);
+        assertEquals(lInitialNbLikes,mWorkmate.getWorkmateLikes().size());
+    }
 
     @Test
-    public void setAndGetWorkmateEmailWithSuccess() { }
+    public void setAndGetWorkmateRestoChosenWithSuccess() {
+        Timestamp lTimestamp = Timestamp.now();
+
+        mWorkmateRestoChosen = new Workmate.WorkmateRestoChoice();
+        assertNotNull(mWorkmateRestoChosen);
+        assertNull(mWorkmateRestoChosen.getRestoId());
+        assertNull(mWorkmateRestoChosen.getRestoName());
+        assertNull(mWorkmateRestoChosen.getRestoDateChoice());
+
+        mWorkmate = new Workmate(mNoId,mNoName, mNoEmail, mNoPhotoUrl,mWorkmateRestoChosen);
+
+        Workmate.WorkmateRestoChoice lSetData = new Workmate.WorkmateRestoChoice("tataid", "tata eugenie", lTimestamp);
+        mWorkmate.setWorkmateRestoChosen(lSetData);
+
+        Workmate.WorkmateRestoChoice lGetData = mWorkmate.getWorkmateRestoChosen();
+        assertNotNull(lGetData);
+        assertEquals(lSetData.getRestoId(), lGetData.getRestoId());
+        assertEquals(lSetData.getRestoName(), lGetData.getRestoName());
+        assertEquals(lSetData.getRestoDateChoice(), lGetData.getRestoDateChoice());
+    }
 
     @Test
-    public void setAndGetWorkmatePhotoUrlWithSuccess() { }
+    public void setAndGetWorkmateLikesRestoIdWithSuccess() {
+        mWorkmateLikes = new Workmate.Likes();
+        assertNull(mWorkmateLikes.getRestoId());
+
+        String lSetData = "tataid";
+        mWorkmateLikes.setRestoId(lSetData);
+
+        String lGetData = mWorkmateLikes.getRestoId();
+        assertNotNull(lGetData);
+        assertEquals(lSetData, lGetData);
+    }
 
     @Test
-    public void setAndGetWorkmateLikesWithSuccess() { }
+    public void setAndGetWorkmateLikesRestoNameWithSuccess() {
+        mWorkmateLikes = new Workmate.Likes();
+        assertNull(mWorkmateLikes.getRestoName());
+
+        String lSetData = "tata eugénie";
+        mWorkmateLikes.setRestoName(lSetData);
+
+        String lGetData = mWorkmateLikes.getRestoName();
+        assertNotNull(lGetData);
+        assertEquals(lSetData, lGetData);
+
+    }
 
     @Test
-    public void setAndGetWorkmateRestoChosenWithSuccess() { }
+    public void setAndGetWorkmateRestoChosenRestoIdWithSuccess() {
+        mWorkmateRestoChosen = new Workmate.WorkmateRestoChoice();
+        assertNotNull(mWorkmateRestoChosen);
+        assertNull(mWorkmateRestoChosen.getRestoId());
+
+        String lSetData = "tataid";
+        mWorkmateRestoChosen.setRestoId(lSetData);
+
+        String lGetData = mWorkmateRestoChosen.getRestoId();
+        assertNotNull(lGetData);
+        assertEquals(lSetData, lGetData);
+
+    }
 
     @Test
-    public void setAndGetWorkmateLikesRestoIdWithSuccess() { }
+    public void setAndGetWorkmateRestoChosenRestoNameWithSuccess() {
+        mWorkmateRestoChosen = new Workmate.WorkmateRestoChoice();
+        assertNotNull(mWorkmateRestoChosen);
+        assertNull(mWorkmateRestoChosen.getRestoName());
+
+        String lSetData = "tata eugénie";
+        mWorkmateRestoChosen.setRestoName(lSetData);
+
+        String lGetData = mWorkmateRestoChosen.getRestoName();
+        assertNotNull(lGetData);
+        assertEquals(lSetData, lGetData);
+    }
 
     @Test
-    public void setAndGetWorkmateLikesRestoNameWithSuccess() { }
+    public void setAndGetWorkmateRestoChosenRestoDateChoiceWithSuccess() {
+        mWorkmateRestoChosen = new Workmate.WorkmateRestoChoice();
+        assertNotNull(mWorkmateRestoChosen);
+        assertNull(mWorkmateRestoChosen.getRestoDateChoice());
 
-    @Test
-    public void setAndGetWorkmateLikesRestoDateChoiceWithSuccess() { }
 
-    @Test
-    public void setAndGetWorkmateRestoChosenRestoIdWithSuccess() { }
+        Timestamp lSetData = Timestamp.now();
+        mWorkmateRestoChosen.setRestoDateChoice(lSetData);
 
-    @Test
-    public void setAndGetWorkmateRestoChosenRestoNameWithSuccess() { }
-
-    @Test
-    public void setAndGetWorkmateRestoChosenRestoDateChoiceWithSuccess() { }
+        Timestamp lGetData = mWorkmateRestoChosen.getRestoDateChoice();
+        assertNotNull(lGetData);
+        assertEquals(lSetData, lGetData);
+    }
 
 }
